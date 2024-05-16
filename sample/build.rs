648b91a11aef68f8554fe3d68310eeb644e53af5
felixmaker <7445790+felixmaker@users.noreply.github.com>
2024-05-16 22:29:47 +0800

    Update sample

--- sample/build.rs
+++ sample/build.rs
@@ -1,11 +1,7 @@
-use thunk::{Subsystem, ThunkBuilder, OS};
+use thunk::{ThunkBuilder, OS};
 
 fn main() {
-    if let Ok(thunk) = ThunkBuilder::default()
-        .with_os(OS::Windows7)
-        .with_subsystem(Subsystem::Windows)
-        .build()
-    {
+    if let Ok(thunk) = ThunkBuilder::default().with_os(OS::WindowsXP).build() {
         thunk.thunk();
     }
 }
