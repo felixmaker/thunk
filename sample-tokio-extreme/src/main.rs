962058eccc5218c0c3e081be068245a83092a0ec
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2024-10-14 22:32:22 +0800

    update extreme example

--- sample-tokio-extreme/src/main.rs
+++ sample-tokio-extreme/src/main.rs
@@ -1,4 +1,9 @@
-#[tokio::main]
+use libc_alloc::LibcAlloc;
+
+#[global_allocator]
+static ALLOCATOR: LibcAlloc = LibcAlloc;
+
+#[tokio::main(flavor = "current_thread")]
 async fn main() {
     println!("Hello, world!");
 }
